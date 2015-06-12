5b67673417075e7e21a3fc6f43b4ea8d75df7b27
Yicheng Sun <yichengs@princeton.edu>
2015-06-12 16:13:26 +0100

    added landscape resources to testapp

--- TestApplication/app/src/main/java/com/example/epic/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/epic/testapplication/MainActivity.java
@@ -2,15 +2,18 @@ package com.example.epic.testapplication;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
 public class MainActivity extends ActionBarActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
     }
 
