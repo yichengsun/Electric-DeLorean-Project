68bbd7b3cb234d34bbe7b7a73cc7e0fbb29acc3b
henryshangguan <henry.shangguan@gmail.com>
2015-07-07 11:48:01 +0100

    added timestamp and trip start/stop robustness

--- TestApplication/app/src/main/java/com/example/epic/testapplication/DataPoint.java
+++ TestApplication/app/src/main/java/com/example/epic/testapplication/DataPoint.java
@@ -2,13 +2,15 @@ package com.example.epic.testapplication;
 
 import com.google.maps.android.geometry.Point;
 
+import java.text.DateFormat;
+
 /**
  * Created by henryshangguan on 6/30/15.
  */
 public class DataPoint {
+    private String timestamp;
     private int route;
     private Point coordinates;
-    private double altitude;
     private double time_elapsed_seconds;
     private double distance_from_prev_miles;
     private double total_distance_miles;
@@ -16,10 +18,10 @@ public class DataPoint {
     private double MPG;
     private double velocity_mph;
 
-    public DataPoint(int route, double lat, double lng, double alt, double timeElapsed, double diff, double dist, double batt, double mpg, double vel) {
+    public DataPoint(String time, int route, double lat, double lng, double timeElapsed, double diff, double dist, double batt, double mpg, double vel) {
+        this.timestamp = time;
         this.route = route;
         this.coordinates = new Point(lat, lng);
-        this.altitude = alt;
         this.time_elapsed_seconds = timeElapsed;
         this.distance_from_prev_miles = diff;
         this.total_distance_miles = dist;
