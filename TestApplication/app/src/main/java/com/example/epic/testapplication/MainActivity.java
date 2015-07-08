0833ff1e67cb657344af0dc27a1f62f2f8d8c3d6
henryshangguan <henry.shangguan@gmail.com>
2015-07-08 17:03:56 +0100

    fixed switch views crash

--- TestApplication/app/src/main/java/com/example/epic/testapplication/MainActivity.java
+++ TestApplication/app/src/main/java/com/example/epic/testapplication/MainActivity.java
@@ -139,6 +139,8 @@ public class MainActivity extends ActionBarActivity {
                     fm.beginTransaction().replace(R.id.mainFragmentContainer, tripFragmentStats).commit();
                     mMapView = false;
                 }
+                return true;
+            git
             case R.id.parse_push:
                 Log.d(TAG, "parse push called");
                 ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
